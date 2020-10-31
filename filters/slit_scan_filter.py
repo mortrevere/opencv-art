15e2c148bc9bb56e2cad0f7b78227702614ee1be
mortrevere <leodroide@hotmail.fr>
2020-10-31 16:36:08 +0100

    remove acapture (wrong colors) and fix slitscan

--- filters/slit_scan_filter.py
+++ filters/slit_scan_filter.py
@@ -1,6 +1,7 @@
 from collections import deque 
 import numpy as np
 import cv2 as cv
+import math
 
 class SlitScanFilter:
     def __init__(self, rows, cols, size=50):
@@ -15,15 +16,14 @@ class SlitScanFilter:
     def compute(self, frame):
         out = self._blank.copy()
         c = 0
-        w = round(self.rows/self.size)
+        w = math.ceil(self.rows/self.size)
+        print("w", w)
         self.queue.pop()
         self.queue.appendleft(frame)
         for f in self.queue:
             mask = self._mask.copy()
-            pos = round(c*self.rows/self.size)
-            
-
-            cv.line(mask,(0,pos),(self.cols,pos),(255,255,255),w-1)
+            pos = math.floor(c*w)
+            cv.rectangle(mask,(0,pos),(self.cols,pos + w - 1),(255,255,255), -1)
             stripe = cv.bitwise_and(f,f,mask=mask)
             out = cv.bitwise_or(stripe, out)
             c += 1
--- main.py
+++ main.py
@@ -1,15 +1,14 @@
-import acapture
 import numpy as np
 import cv2 as cv
 import time
 from performance_watcher import PerformanceWatcher
 from filters import *
 
-cap = acapture.open(2)
+cap = cv.VideoCapture(0)
 ret, frame = cap.read()
 rows, cols, depth = frame.shape
 
-sfilter = slit_scan_filter.SlitScanFilter(rows, cols, 50)
+sfilter = slit_scan_filter.SlitScanFilter(rows, cols, 40)
 perfs = PerformanceWatcher(10)
 
 while True:
@@ -24,7 +23,6 @@ while True:
 
     perfs.observe(time.time() - t1)
 
-    print(perfs.get_fps())
+    print(command, perfs.get_fps())
 
-cap.release()
 cv.destroyAllWindows()
