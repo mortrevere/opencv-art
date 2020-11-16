780705893a11d20870803daa159f78a91605f472
mortrevere <leodroide@hotmail.fr>
2020-11-16 23:58:29 +0100

    add tunnel filter (wip)

--- filters/tunnel.py
+++ filters/tunnel.py
@@ -8,31 +8,33 @@ from oscillator import Oscillator
 class TunnelFilter(Filter):
     def init(self):
         self._previous = self._blank.copy()
-        self.osc_h = Oscillator(freq=1, zero=True)
-        self.osc_w = Oscillator(freq=1 / 3, zero=True, phase=42)
-        self.osc_f = Oscillator(freq=1 / 7, zero=True, phase=42)
+        self.osc_h = Oscillator(freq=5)
+        self.osc_w = Oscillator(freq=16, phase=42)
+        self.osc_f = Oscillator(freq=7, phase=42)
+        self.osc_sign1 = Oscillator(zero=True, freq=1/6)
+        self.osc_sign2 = Oscillator(freq=17, phase=77)
 
     def compute(self, frame):
-        o_h = self.osc_h.next()
-        o_w = self.osc_w.next()
-        o_f = self.osc_f.next()
-        # print(o)
+        m = self.osc_sign1.next()
+        o_h = self.osc_h.next()/20
+        o_w = self.osc_w.next()/20
+
+        # adds zooms
+        # o_f = (self.osc_f.next() * (1-m)) + m
+        o_f = self.osc_f.next() * 0.1 + 0.9
+
         f = frame
 
-        factor = o_f / 2 + 0.5
-        h_translate = o_h * (self.cols * factor) / 8
-        w_translate = o_w * (self.rows * factor) / 8
-        # print(h_translate, w_translate)
+        factor = o_f
+        # adds wiggling
+        h_translate = (1-o_f)/2 * self.cols #+ o_h*120
+        w_translate = (1-o_f)/2 * self.rows #+ o_w*170
         M = np.float32([[factor, 0, h_translate], [0, factor, w_translate]])
-        # f = cv.warpAffine(f, M, (self.cols, self.rows))
-        # f = self._previous + f
-        # f = cv.bitwise_xor(f, self._previous)
-        f = cv.addWeighted(f, 0.6, self._previous, 0.6, 0.0)
+        f = cv.bitwise_xor(f, self._previous)
 
         self._previous = cv.warpAffine(f, M, (self.cols, self.rows))
         imghsv = cv.cvtColor(self._previous, cv.COLOR_BGR2HSV).astype("float32")
         (h, s, v) = cv.split(imghsv)
-
         s = s * (10 * o_f)
         s = np.clip(s, 0, 255)
         imghsv = cv.merge([h, s, v])
